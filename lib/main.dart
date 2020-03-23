a934b39759e7d561c7d9adbdd10c29807579f031
Muhammad Junaid Iqbal <50970445+JunaidIQ@users.noreply.github.com>
2020-03-23 10:59:55 +0500

    8 ball App practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(
-      MaterialApp(
-        home: null,
+import 'dart:math';
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue.shade600,
+        appBar: AppBar(title: Text('8 ball App'),),
+        body: BallApp() ,
       ),
+    ),
+  );
+}
+
+
+class BallApp extends StatefulWidget {
+  @override
+  _BallAppState createState() => _BallAppState();
+}
+
+class _BallAppState extends State<BallApp> {
+  int ballN = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: (){
+              setState(() {
+                ballN = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset("images/ball$ballN.png"),
+          ),
+        ),
+      ],
     );
+  }
+}
